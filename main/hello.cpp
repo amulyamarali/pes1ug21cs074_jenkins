b9193d60c20ec89f65a2b395ecec9480b37a56a3
Amulya Marali <maraliamulya@gmail.com>
2024-03-08 11:25:06 +0530

    task 4 commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins :)\n";
-  cout << "I have successfully built and run\n";
   cout << "i have added a webhook\n";
   return 0;
 }
